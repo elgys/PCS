fd13c623fe92084c730e39c7a1fe9c9033303b8c
Levi van der Griendt <elgys@hotmail.com>
2020-01-10 16:38:55 +0100

    made basic human with center of gravity calc

---
+++ human.py
@@ -0,0 +1,147 @@
+import numpy as np
+import physics as p
+
+
+class human:
+
+    dic = {}
+    def  __init__(self,length,weight,bodyposition,shouldersize):
+        # here the values are given in degrees.
+        self.head = 0
+        self.torso = 0
+        self.rightUpperArm = 0
+        self.rightLowerArm = 0
+        self.rightUpperLeg = 0
+        self.rightLowerLeg = 0
+        self.leftUpperArm = 0
+        self.leftLowerArm = 0
+        self.leftUpperLeg = 0
+        self.leftLowerLeg = 0
+        #length in cm
+        self.length = length
+        #weigth in killo's
+        self.weight = weight
+        # the bodyposition is the posstion of the middle of the body
+        self.bodyposition = np.array(bodyposition)
+        # the midsize of a shoulder
+        self.shoulderSize = shouldersize
+        #if the turner is turned
+        self.turned = False;
+        if "done" not in self.dic.keys():
+            self.dic["done"] = 1
+            f = open("humaninit.txt",'r')
+            for line in f:
+                (key,value) = line.split()
+                self.dic[key] = float(value)
+        self.cog = self.__getCenterOfMass()
+
+    def __rotationBodyParts(self,begin,end,rot):
+        """ Here we can rotate a bodypart without knowing the body part by
+            using a vector caclulated between de diffrence and the start this will give back
+            (vector from start to end, place of end)"""
+        vec = end - begin
+        vec = np.dot(p.rotation2d(rot),vec)
+        end = begin + vec
+        return(vec,end)
+
+    def __legcalc(self,topposition,legrot,Upper=True):
+        """Caclulate the place of Center of mass and place of the end part
+            """
+        if Upper:
+            string = "_Upper_Leg"
+        else:
+            string = "_Lower_Leg"
+
+        lowerPart = topposition - np.array([0,self.dic["length"+string] * self.length])
+        (vec,lowerPart) = self.__rotationBodyParts(topposition,lowerPart,legrot)
+        cog = topposition + vec * self.dic["cog"+ string]
+        mass = self.weight * self.dic["weight"+ string]
+        return (cog,mass,lowerPart)
+
+    def __armcalc(self,topposition,armrot,Upper=True,right = True):
+        """
+        Here we caclulate the arm length and place and cog of arm parts
+        """
+        if Upper:
+            string = "_Upper_Arm"
+        else:
+            string = "_Lower_Arm"
+        if right:
+            lowerPart = topposition - np.array([self.dic["length"+string] * self.length,0])
+        else:
+            lowerPart = topposition + np.array([self.dic["length"+string] * self.length,0])
+        (vec,lowerPart) = self.__rotationBodyParts(topposition,lowerPart,armrot)
+        cog = lowerPart + vec *(1- self.dic["cog"+ string])
+        mass = self.weight * self.dic["weight"+ string]
+        return (cog,mass,lowerPart)
+
+    def __headcalc(self,neck,headrot):
+        tophead = neck + np.array([0,self.length * self.dic["length_Head"]])
+        (vec,tophead) = self.__rotationBodyParts(neck,tophead,self.head)
+        cog = neck + vec * (1 - self.dic["cog_Head"])
+        mass = self.weight * self.dic["weight_Head"]
+        return(cog,mass,tophead)
+
+    def __getCenterOfMass(self):
+        CenterofMass=[]
+        masses = []
+        # here first we caclulate the place of the legs and there center of mass
+        # and put them into
+        legMiddle = self.bodyposition - (np.array([0,self.length * self.dic["length_Body"] /2]))
+        if self.turned:
+            sideLength = [0,0]
+        else:
+            sideLength = [self.shoulderSize/ 2,0]
+        rightLeg = legMiddle - sideLength
+        leftLeg = legMiddle + sideLength
+        # left leg upper caclulations
+        (cogLeftUpperLeg,mass,leftKnee)= self.__legcalc(leftLeg,self.leftUpperLeg)
+        CenterofMass.append(cogLeftUpperLeg)
+        masses.append(mass)
+        #lower left caclulation
+        (cogLeftLowerLeg,mass,leftFoot) = self.__legcalc(leftKnee,self.leftLowerLeg,False)
+        CenterofMass.append(cogLeftLowerLeg)
+        masses.append(mass)
+        #rightUpperLeg caclulations
+        (cogRightUpperLeg,mass,rightKnee)= self.__legcalc(rightLeg,self.rightUpperLeg)
+        CenterofMass.append(cogRightUpperLeg)
+        masses.append(mass)
+        #right LowerLeg
+        (cogRightLowerLeg,mass,rightFoot)= self.__legcalc(rightKnee,self.rightLowerLeg,False)
+        CenterofMass.append(cogRightLowerLeg)
+        masses.append(mass)
+        # now let's caclulate the arms
+        armMiddle = self.bodyposition + (np.array([0,self.length * self.dic["length_Body"] / 2]))
+
+        rightArm = armMiddle - sideLength
+        leftArm = armMiddle + sideLength
+
+        (cogRightUpperArm,mass, rightElbow) = self.__armcalc(rightArm,self.rightUpperArm,True,True)
+        CenterofMass.append(cogRightUpperArm)
+        masses.append(mass)
+        (cogRightLowerArm,mass,righthand) = self.__armcalc(rightElbow,self.rightLowerArm,False,True)
+        CenterofMass.append(cogRightLowerArm)
+        masses.append(mass)
+        (cogLeftUpperArm,mass, rightElbow) = self.__armcalc(rightArm,self.rightUpperArm,True,False)
+        CenterofMass.append(cogRightUpperArm)
+        masses.append(mass)
+
+        (cogRightLowerArm,mass,righthand) = self.__armcalc(rightElbow,self.rightLowerArm,False,False)
+        CenterofMass.append(cogRightLowerArm)
+        masses.append(mass)
+
+        #body cacls
+        vec = legMiddle - armMiddle
+        CenterofMass.append(legMiddle + (vec * self.dic["cog_Body"]))
+        masses.append(self.weight * self.dic["weight_Body"])
+
+        # head caclc
+        (cogHead,mass,head) = self.__headcalc(armMiddle,self.head)
+        print(head)
+        CenterofMass.append(cogHead)
+        masses.append(mass)
+        print(p.getCenterOfMass(CenterofMass,masses))
+        return p.getCenterOfMass(CenterofMass,masses)
+
+#test code
+human = human(180,100,list([0,90+25.2]),20)
