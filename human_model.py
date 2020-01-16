ba4959822e23442f42d05a68488453950da0b617
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2020-01-16 13:12:16 +0100

    style

--- human_model.py
+++ human_model.py
@@ -4,7 +4,7 @@ import physics as p
 class human:
 
     dic = {}
-    def  __init__(self,length,weight,bodyposition,shouldersize):
+    def  __init__(self, length, weight, bodyposition, shouldersize):
         # here the values are given in degrees.
         self.head = 0
         self.torso = 0
@@ -29,25 +29,25 @@ class human:
 
         if "done" not in self.dic.keys():
             self.dic["done"] = 1
-            f = open("humaninit.txt",'r')
+            f = open("humaninit.txt", 'r')
 
             for line in f:
-                (key,value) = line.split()
+                (key, value) = line.split()
                 self.dic[key] = float(value)
 
         self.cog = self.__getCenterOfMass()
 
-    def __rotationBodyParts(self,begin,end,rot):
+    def __rotationBodyParts(self, begin, end, rot):
         """ Rotate a bodypart whithout knowing which bodypart it is, by
             using a vector describing the bodypart.
             It returns (vector, end coordinate)"""
         vec = end - begin
-        vec = np.dot(p.rotation2d(rot),vec)
+        vec = np.dot(p.rotation2d(rot), vec)
         end = begin + vec
 
-        return(vec,end)
+        return(vec, end)
 
-    def __legcalc(self,topposition,legrot,Upper=True):
+    def __legcalc(self, topposition, legrot, Upper=True):
         """ Calculate of the upper or lower leg its end coordinate (knee or
             foot), rotate it and calculate its center of mass and its mass.
             Returns (center of mass, weight, end coordinate)."""
@@ -56,14 +56,14 @@ class human:
         else:
             string = "_Lower_Leg"
 
-        lowerPart = topposition - np.array([0,self.dic["length"+string] * self.length])
-        (vec,lowerPart) = self.__rotationBodyParts(topposition,lowerPart,legrot)
+        lowerPart = topposition - np.array([0, self.dic["length"+string] * self.length])
+        (vec, lowerPart) = self.__rotationBodyParts(topposition, lowerPart, legrot)
         cog = topposition + vec * self.dic["cog"+ string]
         mass = self.weight * self.dic["weight"+ string]
 
-        return (cog,mass,lowerPart)
+        return (cog, mass, lowerPart)
 
-    def __armcalc(self,topposition,armrot,Upper=True,right = True):
+    def __armcalc(self, topposition, armrot, Upper=True, right=True):
         """ Calculate of the upper or lower arm its end coordinate (elbow or
             hand), rotate it and calculate its center of mass and its mass.
             Returns (center of mass, weight, end coordinate)."""
@@ -73,26 +73,26 @@ class human:
             string = "_Lower_Arm"
 
         if right:
-            lowerPart = topposition - np.array([self.dic["length"+string] * self.length,0])
+            lowerPart = topposition - np.array([self.dic["length"+string] * self.length, 0])
         else:
-            lowerPart = topposition + np.array([self.dic["length"+string] * self.length,0])
+            lowerPart = topposition + np.array([self.dic["length"+string] * self.length, 0])
 
-        (vec,lowerPart) = self.__rotationBodyParts(topposition,lowerPart,armrot)
+        (vec, lowerPart) = self.__rotationBodyParts(topposition, lowerPart, armrot)
         cog = lowerPart + vec *(1- self.dic["cog"+ string])
         mass = self.weight * self.dic["weight"+ string]
 
-        return (cog,mass,lowerPart)
+        return (cog, mass, lowerPart)
 
-    def __headcalc(self,neck,headrot):
+    def __headcalc(self, neck, headrot):
         """ Calculate of the head its end coordinate (top of a head),
             rotates it and calculate its center of mass and its mass.
             Returns (center of mass, weight, end coordinate)."""
-        tophead = neck + np.array([0,self.length * self.dic["length_Head"]])
-        (vec,tophead) = self.__rotationBodyParts(neck,tophead,self.head)
+        tophead = neck + np.array([0, self.length * self.dic["length_Head"]])
+        (vec, tophead) = self.__rotationBodyParts(neck, tophead, self.head)
         cog = neck + vec * (1 - self.dic["cog_Head"])
         mass = self.weight * self.dic["weight_Head"]
 
-        return(cog,mass,tophead)
+        return(cog, mass, tophead)
 
     def __getCenterOfMass(self):
         """ Calculate the center of mass of the total body.
@@ -102,64 +102,64 @@ class human:
         CenterofMass=[]
         masses = []
         # Calculate the placement of the legs and their combined center of mass.
-        legMiddle = self.bodyposition - (np.array([0,self.length * self.dic["length_Body"] /2]))
-        (vec,legMiddle)= self.__rotationBodyParts(self.bodyposition,legMiddle,self.torso)
+        legMiddle = self.bodyposition - (np.array([0, self.length * self.dic["length_Body"] /2]))
+        (vec, legMiddle)= self.__rotationBodyParts(self.bodyposition, legMiddle, self.torso)
 
         if self.turned:
-            sideLength = [0,0]
+            sideLength = [0, 0]
         else:
-            sideLength = [self.shoulderSize/ 2,0]
+            sideLength = [self.shoulderSize/ 2, 0]
 
         rightLeg = legMiddle - sideLength
         leftLeg = legMiddle + sideLength
         self.locationRightHip = rightLeg
         self.locationLeftHip = leftLeg
         # left upper leg caclulations
-        (cogLeftUpperLeg,mass,leftKnee)= self.__legcalc(leftLeg,self.leftUpperLeg)
+        (cogLeftUpperLeg, mass, leftKnee)= self.__legcalc(leftLeg, self.leftUpperLeg)
         self.locationLeftKnee = leftKnee
         CenterofMass.append(cogLeftUpperLeg)
         masses.append(mass)
         # left lower leg caclulations
-        (cogLeftLowerLeg,mass,leftFoot) = self.__legcalc(leftKnee,self.leftLowerLeg,False)
+        (cogLeftLowerLeg, mass, leftFoot) = self.__legcalc(leftKnee, self.leftLowerLeg, False)
         self.locationLeftFoot = leftFoot
         CenterofMass.append(cogLeftLowerLeg)
         masses.append(mass)
         # right upper leg caclulations
-        (cogRightUpperLeg,mass,rightKnee)= self.__legcalc(rightLeg,self.rightUpperLeg)
+        (cogRightUpperLeg, mass, rightKnee)= self.__legcalc(rightLeg, self.rightUpperLeg)
         self.locationRightKnee = rightKnee
         CenterofMass.append(cogRightUpperLeg)
         masses.append(mass)
         # right lower leg calculations
-        (cogRightLowerLeg,mass,rightFoot)= self.__legcalc(rightKnee,self.rightLowerLeg,False)
+        (cogRightLowerLeg, mass, rightFoot)= self.__legcalc(rightKnee, self.rightLowerLeg, False)
         self.locationRightFoot = rightFoot
         CenterofMass.append(cogRightLowerLeg)
         masses.append(mass)
 
         # Calculate the placement of the arms and their combined center of mass.
-        armMiddle = self.bodyposition + (np.array([0,self.length * self.dic["length_Body"] / 2]))
-        (vec,armMiddle) = self.__rotationBodyParts(self.bodyposition,armMiddle,self.torso)
+        armMiddle = self.bodyposition + (np.array([0, self.length * self.dic["length_Body"] / 2]))
+        (vec, armMiddle) = self.__rotationBodyParts(self.bodyposition, armMiddle, self.torso)
         rightArm = armMiddle - sideLength
         leftArm = armMiddle + sideLength
         self.locationRightShoulder = rightArm
         self.locationLeftShoulder = leftArm
 
         # right upper arm calculations
-        (cogRightUpperArm,mass, rightElbow) = self.__armcalc(rightArm,self.rightUpperArm,True,True)
+        (cogRightUpperArm, mass, rightElbow) = self.__armcalc(rightArm, self.rightUpperArm, True, True)
         self.locationRightElbow = rightElbow
         CenterofMass.append(cogRightUpperArm)
         masses.append(mass)
         # right lower arm calculations
-        (cogRightLowerArm,mass,righthand) = self.__armcalc(rightElbow,self.rightLowerArm,False,True)
+        (cogRightLowerArm, mass, righthand) = self.__armcalc(rightElbow, self.rightLowerArm, False, True)
         self.locationRightHand = righthand
         CenterofMass.append(cogRightLowerArm)
         masses.append(mass)
         # left upper arm calculations
-        (cogLeftUpperArm,mass, leftElbow) = self.__armcalc(leftArm,self.leftUpperArm,True,False)
+        (cogLeftUpperArm, mass, leftElbow) = self.__armcalc(leftArm, self.leftUpperArm, True, False)
         self.locationLeftElbow = leftElbow
         CenterofMass.append(cogLeftUpperArm)
         masses.append(mass)
         # left lower arm calculations
-        (cogLeftLowerArm,mass,lefthand) = self.__armcalc(leftElbow,self.leftLowerArm,False,False)
+        (cogLeftLowerArm, mass, lefthand) = self.__armcalc(leftElbow, self.leftLowerArm, False, False)
         self.locationLeftHand = lefthand
         CenterofMass.append(cogLeftLowerArm)
         masses.append(mass)
@@ -170,14 +170,16 @@ class human:
         masses.append(self.weight * self.dic["weight_Body"])
 
         # head calculations
-        (cogHead,mass,head) = self.__headcalc(armMiddle,self.head)
+        (cogHead, mass, head) = self.__headcalc(armMiddle, self.head)
         self.locationHead = head
         CenterofMass.append(cogHead)
         masses.append(mass)
 
-        return p.getCenterOfMass(CenterofMass,masses)
+        return p.getCenterOfMass(CenterofMass, masses)
 
-    def positionchange(self,head=0,torso=0,leftupperarm=0,leftlowerarm=0,rightupperarm=0,rightlowerarm=0,leftupperleg=0,leftlowerleg=0,rightupperleg=0,rightlowerleg=0):
+    def positionchange(self, head=0, torso=0, leftupperarm=0, leftlowerarm=0,
+                       rightupperarm=0, rightlowerarm=0, leftupperleg=0,
+                       leftlowerleg=0, rightupperleg=0, rightlowerleg=0):
         """ Change the position of the bodyparts in radians and calculate
             its new center of mass."""
         self.head = head * (2*np.pi/360)
@@ -193,7 +195,7 @@ class human:
         self.__getCenterOfMass()
 
 
-    def setturned(self,value):
+    def setturned(self, value):
         """ Change the viewing direction of the gymnast and calculate its
             new center of mass.
             False: facing the screen
@@ -209,7 +211,7 @@ class human:
         """ get the mass of the human."""
         return self.weight
 
-    def rightFootOnMiddel(self,rightpos):
+    def rightFootOnMiddel(self, rightpos):
         """ Calculte from the position of the right foot the center of the
             body."""
         translatie = rightpos - self.locationRightFoot
@@ -218,10 +220,10 @@ class human:
 
     def test_bodypositions(self):
         """ Get the bodypart locations."""
-        array = [self.locationHead,self.locationLeftShoulder,self.locationLeftElbow,
-                 self.locationLeftHand,self.locationRightShoulder,self.locationRightElbow,
-                 self.locationRightHand,self.locationLeftHip,self.locationLeftKnee,
-                 self.locationLeftFoot,self.locationRightHip,self.locationRightKnee,
+        array = [self.locationHead, self.locationLeftShoulder, self.locationLeftElbow,
+                 self.locationLeftHand, self.locationRightShoulder, self.locationRightElbow,
+                 self.locationRightHand, self.locationLeftHip, self.locationLeftKnee,
+                 self.locationLeftFoot, self.locationRightHip, self.locationRightKnee,
                  self.locationRightFoot]
 
         return array
