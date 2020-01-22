61fba78c110ff552d461e6c5b1e9adab5036d3b7
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2020-01-22 16:36:39 +0100

    changed things for plotting

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import numpy as np
 import argparse
 
 
-def results(file):
+def results(file, simul):
     with open(file, 'w+') as f:
-        for i in np.logspace(3, 6, 50):
-            for j in np.logspace(3, 6, 50):
+        for i in np.linspace(1000, 50000, 50):
+            for j in np.linspace(1000, 50000, 50):
                 f.write(str(i) + ' ' + str(j) + ' ' + str(simul.run_simulation(
                     variables={'power1': i, 'power2': j})) + '\n')
 
@@ -14,23 +14,35 @@ def results(file):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description='Process an exercise according to a given .exc file.')
+        description='Process an exercise according to a given .exc file.\n\
+                     Standard the exercise is run with forces from the arm and leg\n\
+                     ranging from 1000 to 50000.')
     parser.add_argument('--file', type=str, default='./exercises/T_pose.exc',
                         help='The name of the file to be simulated (default=%(default)s)')
     parser.add_argument('-t', type=int, default=1, metavar='N',
-                        help='Amount of times to simulate the current file (default=%(default)d)')
-    parser.add_argument('-r', type=str, default='./results/default_results.res',
-                        help='The place where you want to store your results (default=%(default)s).\n\
-                        Also if this not set it will not save the results')
+                        help='Amount of times to simulate the current file per power pair.\n\
+                        After some testing, the models seem deterministic, \n\
+                        so more than once should not be necessary.\n\
+                        (default=%(default)d)')
+    parser.add_argument('-r', type=str, default='',
+                        help='The place where you want to store your results (default=<name exc file>.res')
     parser.add_argument('--simul_time', type=float, default=20.0,
                         help='The simulation time in seconds every run gets before resulting in failure (default=%(default)d)')
     parser.add_argument('-v', action='store_true',
-                        help='Turn visualization on (default=%(default)d)')
+                        help='Turn visualization on')
+    parser.add_argument('-s', action='store_true',
+                        help='Only simulate a single scenario and do not save results.')
 
     args = parser.parse_args()
 
     simul = inter.Exercise_simulator(
         args.file, args.t, args.simul_time, visual=args.v)
 
-    if args.r is not "":
-        results(args.r)
+    if args.r == '':
+        args.r = './results/' + args.file.split('/')[-1][:-4] + '.res'
+
+    if not args.s:
+        results(args.r, simul)
+    else:
+        print(simul.run_simulation(
+            variables={'power1': 25000, 'power2': 25000}) / np.pi * 180)
