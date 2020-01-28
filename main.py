b1bd9904f17b947e960fd4c3232195a883ef4011
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2020-01-28 14:30:38 +0100

    fixed main standard run, gitignore now not ign map

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def multirun(file, simul, functions, minn=1000, maxi=50000):
             for j in np.linspace(minn, maxi, 50):
                 print(str(i) + str(j))
 
-                simul.reset(variables = {'power1':i,'power2': j})
-                f.write(str(i) + ' ' + str(j) + ' ' + str(run(simul,functions)) + '\n')
+                simul.reset(variables={'power1': i, 'power2': j})
+                f.write(str(i) + ' ' + str(j) + ' ' +
+                        str(run(simul, functions)) + '\n')
 
 
 def run(simul, functions):
@@ -20,7 +21,7 @@ def run(simul, functions):
         for func in functions:
             func()
 
-    return simul.get_results();
+    return simul.get_results()
 
 
 if __name__ == "__main__":
@@ -40,17 +41,19 @@ if __name__ == "__main__":
                         Also if this not set it will not save the results')
     parser.add_argument('--simul_time', type=float, default=20.0,
                         help='The simulation time in seconds every run gets before resulting in failure (default=%(default)d)')
-    parser.add_argument('--human_size',type=int, default = 160,
+    parser.add_argument('--human_size', type=int, default=160,
                         help='This will give the humans size that be used for the sim. (default=%(default)d)')
-    parser.add_argument('--human_weight',type=int,default=55,
+    parser.add_argument('--human_weight', type=int, default=55,
                         help='This will give the weight of a human (default=%(default)d)')
-    parser.add_argument('-d','--debug_view',action='store_true',help ='Here we will call \
+    parser.add_argument('-d', '--debug_view', action='store_true', help='Here we will call \
                         upon the of pygame is usefull for debug drawing')
+    parser.add_argument('-s', action='store_true',
+                        help='Run the simulation only once, do not save the results.')
 
     args = parser.parse_args()
     functionlist = []
     simul = sim.simulator(args.file, args.t, args.simul_time, args.human_size,
-                            args.human_weight)
+                          args.human_weight)
 
     if args.debug_view:
         import debug
@@ -58,8 +61,10 @@ if __name__ == "__main__":
         deb = debug.Debug(simul)
         functionlist.append(deb.step_draw)
 
-    if args.r is not "":
+    if args.r is "":
         args.r = 'results/' + args.file.split('/')[-1][:-4] + '.res'
+
+    if not args.s:
         multirun(args.r, simul, functionlist)
     else:
         run(simul, functionlist)
